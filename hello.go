e290fd1e11acf60f047237802872003429cba037
Thor10-code <99094252+Thor10-code@users.noreply.github.com>
2022-08-04 01:10:43 +0400

    Add files via upload

--- hello.go
+++ hello.go
@@ -1,6 +1,25 @@
-package hello
+package main
+
 import "fmt"
 
-func main(){
-	fmt.Println("Hello world")
+func guessGame() {
+	magicNum := 34
+	chance := 3
+	var guess int
+	var try int
+	for guess != magicNum && try != chance {
+		fmt.Println("Write your guess:")
+		fmt.Scan(&guess)
+		try++
+	}
+	if magicNum != guess {
+		fmt.Printf("My guess was %v\n", magicNum)
+	} else {
+		fmt.Println("Congralutations!")
+	}
+
+}
+
+func main() {
+	guessGame()
 }
